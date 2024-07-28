f8eda2afb9b44a81d2f43ffef4c96daeef1ceb26
manastelavane777 <manastelavane@ternaengg.ac.in>
2024-07-28 12:12:23 +0530

    FIX:cleaning the project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import dpsLogo from './assets/DPS.svg';
 import './App.css';
 
 function App() {
-	return (
-		<>
-			<div>
-				<a href="https://www.digitalproductschool.io/" target="_blank">
-					<img src={dpsLogo} className="logo" alt="DPS logo" />
-				</a>
-			</div>
-			<div className="home-card">
-				<p>Your solution goes here 😊</p>
-			</div>
-		</>
-	);
+	return <div></div>;
 }
 
 export default App;
